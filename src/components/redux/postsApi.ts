3686e5aeb403d713a0415f360525f5b8d24f953f
Pantheleymon <pantheleymonbbx@yandex.ru>
2024-03-30 12:21:06 +0300

    feat: RTK Query

---
+++ src/components/redux/postsApi.ts
@@ -0,0 +1,22 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+interface IPost {
+    id: string;
+    title: string;
+    body: string;
+}
+
+export const postsApi = createApi({
+    reducerPath: 'postsApi',
+    baseQuery: fetchBaseQuery({baseUrl: 'https://jsonplaceholder.typicode.com/'}),
+    endpoints: (builder) => ({
+        getPosts: builder.query<Array<IPost>, {}>({
+            query: () => `posts`,
+        }),
+        getPostById: builder.query<IPost, number>({
+            query: (id) => `posts/${id}`,
+        })
+    })
+});
+
+export const {useGetPostsQuery, useLazyGetPostByIdQuery} = postsApi;
