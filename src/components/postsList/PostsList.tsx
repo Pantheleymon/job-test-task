fce2f6f49e6693b7f0b23da0033caaadc739e9b1
Pantheleymon <pantheleymonbbx@yandex.ru>
2024-03-30 13:40:58 +0300

    feat: Поиск по постам

--- src/components/postsList/PostsList.tsx
+++ src/components/postsList/PostsList.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import { useGetPostsQuery } from "../redux";
 import Spinner from "../spinner/Spinner";
+import { useState } from "react";
 
 const PostsList = () => {
+    const [searchTerm, setSearchTerm] = useState<string>('');
+
     const {data = [], isLoading, isError} = useGetPostsQuery('/');
 
     if (isLoading) {
@@ -11,19 +14,31 @@ const PostsList = () => {
     if (isError) {
       return <>'Ошибка загрузки'</>;
     }
+
+    const filteredPosts = data
+                            .filter((item) => {
+                                return item.title.includes(searchTerm);
+                            })
+                            .map((item) => {
+                                return (
+                                <div key={item.id}>
+                                    <Link to={`/posts/${item.id}`}>
+                                    <img src="https://placehold.co/600x400/orange/white" alt={item.title} />
+                                    <h2>{item.title}</h2>
+                                    <p>{item.body}</p>
+                                    </Link>
+                                </div>
+                                )
+                            });
+                        
   
     return (
-      <>{data.map((item) => {
-        return (
-          <div key={item.id}>
-            <Link to={`/posts/${item.id}`}>
-              <img src="https://placehold.co/600x400/orange/white" alt={item.title} />
-              <h2>{item.title}</h2>
-              <p>{item.body}</p>
-            </Link>
-          </div>
-        )
-      })}</>
+      <>
+      <input type="text" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)}/>
+
+      {filteredPosts}
+      
+      </>
     );
 };
 
